40c7627efa2ab67edb15e0a03787dbcbb7e0ff39
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-05-10 07:49:19 +0300

    Update Task.hpp

--- Source Code/Task.hpp
+++ Source Code/Task.hpp
@@ -1,35 +1,24 @@
 #include <iostream>
-#include <sstream>
-
-#ifdef _WIN32
-#include <windows.h>          //Used when starting a Windows Task calling ShellExecute function from Windows API
-#endif
-
-
 
 class Task{
 	private:
 		std::string Name;
 		std::string Location;
-	
 		
 	public:
-		Task();
-		Task(const char* ,const char* );  
+		Task(const char* ,const char* );   
 		Task(const char*);                //Constructor used when reading saved data from a file       
 		
-		void start();       //Start Application               
+		void start();       //Start Task               
 		
 		/*Update Name/Location*/
-		void update();
-		
-		void update_Name(const char* );
-		void update_Location(const char* );
+		void update_Name(const char*);
+		void update_Location(const char*);
 		
 		/*Getters*/
-		std::string get_Name(void);
-		std::string get_Location(void);
+		std::string get_Name();
+		std::string get_Location();
 		
 		/*Convert to string for saving purposes*/
-		std::string toString(void);
+		std::string toString();
 };
