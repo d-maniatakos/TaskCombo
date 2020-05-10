9366474a1270d1d895ff158d8cdbe0b1c4fa382b
Dionysis Maniatakos <64664263+d-maniatakos@users.noreply.github.com>
2020-05-10 07:51:09 +0300

    Update Session.hpp

--- Source Code/Session.hpp
+++ Source Code/Session.hpp
@@ -1,6 +1,5 @@
 #include <iostream> 
 #include <list> 
-#include <iterator>
 #include "Task.hpp"
 
 class Session{
@@ -9,24 +8,23 @@ class Session{
 		std::list <Task*> Tasks;
 		
 	public:
-		Session();	
-		Session(const char*);		//Constructor used when reading saved data from a file	
+		Session(const char*);	
+		Session(const char*,bool);		//Constructor used when reading saved data from a file	
 		~Session();
 		
-		void Console();
+		void start_Session();
+		std::list<Task*> return_Tasks();
+		Task * return_Task(int);
 		
-		void Start_Session();
-		void Show_Tasks();
-		
-		void Add_Task();
+		void add_Task(const char*,const char*);
+		void remove_Task(int);
 		
 		std::string get_Name();
-		void update_Name();
+		void update_Name(const char*);
 		
 		void Clear();
 		
 		/*Convert to string for saving purposes*/
 		std::string toString();
 		
-};
-	
+};	
